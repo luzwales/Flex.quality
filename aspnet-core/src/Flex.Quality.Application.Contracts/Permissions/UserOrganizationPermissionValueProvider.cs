aa0e27a952a394420ba8f2b05ae01c48940a0c25
luzwales <luzwales@163.com>
2025-02-13 18:56:04 +0800

    feat(权限管理): 实现基于用户组织的权限验证
    - 新增 GetOrganizationUnitByUserAsync 方法获取用户所属组织单元
    - 实现 UserOrganizationPermissionValueProvider 类进行权限验证
    - 添加必要的数据库引用和接口扩展feat(Organization): 根据用户名获取组织单元信息
    
    - 新增 GetOrganizationUnitByUserAsync 方法，支持根据用户名获取组织单元信息- 修改 GetOrganizationUnitByUserAsync 方法，使用新的用户名查询逻辑- 更新 UserOrganizationPermissionValueProvider，使用新的组织单元查询方式

--- aspnet-core/src/Flex.Quality.Application.Contracts/Permissions/UserOrganizationPermissionValueProvider.cs
+++ aspnet-core/src/Flex.Quality.Application.Contracts/Permissions/UserOrganizationPermissionValueProvider.cs
@@ -1,19 +1,54 @@
-﻿using JetBrains.Annotations;
+﻿using Flex.Quality.Admin;
+using Flex.Quality.EntityFrameworkCore;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Volo.Abp;
+using Volo.Abp.Security.Claims;
 
 namespace Flex.Quality.Permissions;
 
-public class UserOrganizationPermissionValueProvider([NotNull] IPermissionStore permissionStore)
+public class UserOrganizationPermissionValueProvider(
+    [NotNull] IPermissionStore permissionStore,
+    IOrganizationAppService organizationAppService,
+    QualityDbContext dbContext)
     : PermissionValueProvider(permissionStore)
 {
     public override string Name { get; } = "O";
 
-    public override Task<PermissionGrantResult> CheckAsync(PermissionValueCheckContext context)
+    public override async Task<PermissionGrantResult> CheckAsync(PermissionValueCheckContext context)
     {
-        throw new NotImplementedException();
+        // 1、获取用户邮箱
+        var name = context.Principal?.FindFirst(AbpClaimTypes.Name)?.Value;
+        var user = dbContext.Users.Include(identityUser => identityUser.OrganizationUnits)
+            .FirstOrDefault(x => x.UserName == name);
+        var organization = user?.OrganizationUnits.FirstOrDefault()?.OrganizationUnitId;
+        var userOrganization = organizationAppService.GetOrganizationUnitByUserAsync(name);
+
+        if (user == null)
+            // 2、返回授权失败
+            return await Task.FromResult(PermissionGrantResult.Undefined);
+
+        // 3、根据邮箱查询UsermManage权限
+        // 1、获取的是接口权限。
+        // 2、UserEamil
+        // 3、具体邮箱
+        // 核心鉴权：查询授权数据库是否有权限。
+        var flag = await PermissionStore.IsGrantedAsync(context.Permission.Name, Name,
+                                                        userOrganization.Result.DisplayName);
+
+        return await Task.FromResult(flag
+                                         ? PermissionGrantResult.Granted
+                                         : PermissionGrantResult.Undefined);
     }
 
-    public override Task<MultiplePermissionGrantResult> CheckAsync(PermissionValuesCheckContext context)
+    public override async Task<MultiplePermissionGrantResult> CheckAsync(PermissionValuesCheckContext context)
     {
-        throw new NotImplementedException();
+        var permissionNames = context.Permissions.Select(x => x.Name).Distinct().ToArray();
+        Check.NotNullOrEmpty(permissionNames, nameof(permissionNames));
+
+        var Email = context.Principal?.FindFirst(AbpClaimTypes.Email)?.Value;
+        if (Email == null) return new MultiplePermissionGrantResult(permissionNames);
+
+        return await PermissionStore.IsGrantedAsync(permissionNames, Name, Email);
     }
 }
--- aspnet-core/src/Flex.Quality.Application/Admin/OrganizationAppService.cs
+++ aspnet-core/src/Flex.Quality.Application/Admin/OrganizationAppService.cs
@@ -1,6 +1,5 @@
 ﻿using Lion.AbpPro.BasicManagement.OrganizationUnits.Dto;
 using Microsoft.Extensions.DependencyInjection;
-using Volo.Abp;
 using Volo.Abp.Domain.Repositories;
 using Volo.Abp.Identity;
 using Volo.Abp.ObjectMapping;
@@ -10,14 +9,46 @@ namespace Flex.Quality.Admin;
 [Dependency(ServiceLifetime.Singleton)]
 public class OrganizationAppService(
     IRepository<OrganizationUnit, Guid> organizationUnitRepository,
-    IObjectMapper objectMapper)
-    : IOrganizationAppService, IRemoteService
+    IObjectMapper objectMapper,
+    IRepository<IdentityUser, Guid> userRepository,
+    IRepository<IdentityUserOrganizationUnit> userOrgRepository,
+    IOrganizationAppService organizationAppServiceImplementation)
+    : IOrganizationAppService
 {
+    private IOrganizationAppService _organizationAppServiceImplementation { get; set; }
+
     public async Task<List<OrganizationUnitDto>> GetAllChildrenRecursiveAsync(Guid parentId)
     {
         var result = new List<OrganizationUnitDto>();
         await GetChildrenRecursiveAsync(parentId, result);
         return result;
+    }
+
+
+    public async Task<OrganizationUnitDto> GetOrganizationUnitByUserAsync(string userName)
+    {
+        var userId = userRepository.FirstOrDefaultAsync(x => x.Name == userName).Result?.Id;
+        var orgUser = userOrgRepository.FirstOrDefaultAsync(x => x.UserId == userId);
+
+        var orgUserId = orgUser.Result?.OrganizationUnitId;
+        var organizationUnit =
+            await organizationUnitRepository.FirstOrDefaultAsync(x => x.Id == orgUserId);
+        if (organizationUnit == null) throw new Exception("OrganizationUnit not found for the user.");
+
+        return objectMapper.Map<OrganizationUnit, OrganizationUnitDto>(organizationUnit);
+    }
+
+    public async Task<OrganizationUnitDto> GetOrganizationUnitByUserAsync(Guid userId)
+    {
+        var user = await userRepository.GetAsync(userId);
+        var orgUserId =
+            userOrgRepository.FirstOrDefaultAsync(x => x.UserId == userId).Result?.OrganizationUnitId;
+        var organizationUnit =
+            await organizationUnitRepository.FirstOrDefaultAsync(x => x.Id == orgUserId);
+
+        if (organizationUnit == null) throw new Exception("OrganizationUnit not found for the user.");
+
+        return objectMapper.Map<OrganizationUnit, OrganizationUnitDto>(organizationUnit);
     }
 
     private async Task GetChildrenRecursiveAsync(Guid parentId, List<OrganizationUnitDto> result)
